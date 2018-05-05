65121a9603df4cb9e05f39742b9adc79d2513ed7
SAITO Hiroshi <h1r0sh1.sa1t0w@gmail.com>
2018-05-06 01:58:19 +0900

    change theme to pure

--- .conf/fish/aliases.fish
+++ .conf/fish/aliases.fish
@@ -2,5 +2,8 @@
 balias pg 'peco_select_ghq_repository'
 balias pz 'peco_select_z_recentd'
 
+# change directory upper
+balias cdp 'bd'
+
 # Git
 balias g 'git'
--- .conf/fish/config.fish
+++ .conf/fish/config.fish
@@ -1,8 +1,20 @@
 set -g theme_date_format "+%H:%M:%S"
 
+# google cloud sdk
+source $HOME/google/google-cloud-sdk/path.fish.inc
+
+# gopath
+set -x GOPATH $HOME/.gopath
+set -x PATH $GOPATH/bin $PATH
+
+# anyenv
+set -x PATH $HOME/.anyenv/bin $PATH
+bass 'eval "$(anyenv init -)"'
+
 # direnv
 eval (direnv hook fish)
 
-source ~/.ghq/github.com/saizz/dotfiles/.conf/fish/functions.fish
-source ~/.ghq/github.com/saizz/dotfiles/.conf/fish/aliases.fish
-source ~/.ghq/github.com/saizz/dotfiles/.conf/fish/keybindings.fish
+source $HOME/.ghq/github.com/saizz/dotfiles/.conf/fish/functions.fish
+source $HOME/.ghq/github.com/saizz/dotfiles/.conf/fish/aliases.fish
+source $HOME/.ghq/github.com/saizz/dotfiles/.conf/fish/keybindings.fish
+
