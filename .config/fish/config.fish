af7b16fb6bda6c91395e1fdd784281d4914fd2a7
SAITO Hiroshi <h1r0sh1.sa1t0w@gmail.com>
2018-05-06 15:52:19 +0900

    chnge dir .conf to .config

---
+++ .config/fish/config.fish
@@ -0,0 +1,20 @@
+set -g theme_date_format "+%H:%M:%S"
+
+# google cloud sdk
+source $HOME/opt/google/google-cloud-sdk/path.fish.inc
+
+# gopath
+set -x GOPATH $HOME/.gopath
+set -x PATH $GOPATH/bin $PATH
+
+# anyenv
+set -x PATH $HOME/.anyenv/bin $PATH
+bass 'eval "$(anyenv init -)"'
+
+# direnv
+eval (direnv hook fish)
+
+source $HOME/.ghq/github.com/saizz/dotfiles/.config/fish/functions.fish
+source $HOME/.ghq/github.com/saizz/dotfiles/.config/fish/aliases.fish
+#source $HOME/.ghq/github.com/saizz/dotfiles/.config/fish/keybindings.fish
+
--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cd
 ln -si ~/.ghq/github.com/saizz/dotfiles/.direnvrc .direnvrc
 ln -si ~/.ghq/github.com/saizz/dotfiles/.spacemacs .spacemacs
 
-cd ~/.conf/fish
-ln -si ~/.ghq/github.com/saizz/dotfiles/.conf/fish/fishfile fishfile
-ln -si ~/.ghq/github.com/saizz/dotfiles/.conf/fish/config.fish config.fish
+cd ~/.config/fish
+ln -si ~/.ghq/github.com/saizz/dotfiles/.config/fish/fishfile fishfile
+ln -si ~/.ghq/github.com/saizz/dotfiles/.config/fish/config.fish config.fish
 
