992c0f7fd21023bb29bb1d063939e941892329ef
SAITO Hiroshi <h1r0sh1.sa1t0w@gmail.com>
2018-05-26 23:02:04 +0900

    over write function by my_fish_prompt

--- .config/fish/functions.fish
+++ .config/fish/functions.fish
@@ -2,7 +2,3 @@ function fzf_select_z_recentd
   z -l | fzf | awk '{ print $2 }' | read recentd
   cd $recentd
 end
-
-function fish_prompt
-    powerline-shell --shell bare $status
-end
--- .config/fish/my_fish_prompt.fish
+++ .config/fish/my_fish_prompt.fish
@@ -1,3 +1,4 @@
+# over write fish_prompt function
 function fish_prompt
     powerline-shell --shell bare $status
 end
