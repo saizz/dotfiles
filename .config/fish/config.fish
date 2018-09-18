0904bca76ef5cc30b4248f7e50987e13ecb63ecc
Hiroshi Saito <h1r0sh1.sa1t0w@gmail.com>
2018-09-18 15:12:40 +0900

    modified fish configure

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,8 @@
-# omf/bobthefish
-set -g theme_date_format "+%H:%M:%S"
+# install fisherman at once
+if not test -f ~/.config/fish/functions/fisher.fish
+  curl -sLo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
+  fisher
+end
 
 # ghq
 set GHQ_SELECTOR fzf
@@ -7,20 +10,8 @@ set GHQ_SELECTOR fzf
 # fzf
 set FZF_LEGACY_KEYBINDINGS 0
 
-# google cloud sdk
-source $HOME/opt/google-cloud-sdk/path.fish.inc
-
-# gopath
-set -x GOPATH $HOME/.gopath
-set -x PATH $GOPATH/bin $PATH
-
-# anyenv
-set -x PATH $HOME/.anyenv/bin $PATH
-bass 'eval "$(anyenv init -)"'
-
-# direnv
-eval (direnv hook fish)
-
 source $HOME/.ghq/github.com/saizz/dotfiles/.config/fish/functions.fish
 source $HOME/.ghq/github.com/saizz/dotfiles/.config/fish/aliases.fish
 source $HOME/.ghq/github.com/saizz/dotfiles/.config/fish/my_fish_prompt.fish
+
+set -x PIPENV_VENV_IN_PROJECT true
